550cb40d69d0cf4792e335041299f81b43ba0056
Yevhen Kuzminov <kyzminov@gmail.com>
2020-01-15 17:01:40 +0200

    basic routing and logging

--- blog.rb
+++ blog.rb
@@ -8,12 +8,29 @@ params = {
 tcp_server = TCPServer.new(params[:host], params[:port])
 
 while session = tcp_server.accept
-  puts "#{Time.now}: TCP request started"
+  request_start_time = Time.now
+  request = session.gets
+  puts request
+  verb, path, http_version = request.split(' ')
 
-  file = File.read('./data.html')
+  if verb == 'GET' && path == '/'
+    file_path = './data/index.html'
+    response_code = 200
+  elsif verb == 'GET' && path.start_with?('/page/')
+    file_name = path.sub '/page/', ''
+    file_path = "./data/#{file_name}.html"
+    response_code = 200
+  else
+    file_path = './data/error-404.html'
+    response_code = 404
+  end
 
-  session.puts "HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n\ #{file}"
+  file = File.read(file_path)
+
+  response = "#{http_version} #{response_code}\r\nContent-Type: text/html\r\n\r\n\ #{file}"
+  session.puts response
   session.close
 
-  puts "#{Time.now}: TCP request handled"
+  request_duration_ms = ((Time.now.to_f - request_start_time.to_f) * 1000).round(4)
+  puts "#{Time.now}, #{verb}, #{path}, #{response_code}, #{request_duration_ms} ms"
 end
